ff394d3d632b875a57383bb5bfb23d3c95640d49
taevic96 <72222453+taevic96@users.noreply.github.com>
2020-10-01 11:16:11 -0700

    Add files via upload

---
+++ first-homework.py
@@ -0,0 +1,8 @@
+#1/usr/bin/env python3
+
+#This is the first-homework
+#and it will print my full name 
+#and preferred pronouns.
+
+print("Name: Vicente Pena Perez") # print out my full name
+print("Preferred pronouns: he/him") # print out my preferred pronouns
